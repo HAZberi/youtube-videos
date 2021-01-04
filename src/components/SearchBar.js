9497431fd0bbbd0a478e729fb283025180d9252c
HAZberi <mhazuberi@gmail.com>
2021-01-04 16:15:56 -0700

    SearchBar To Hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { searchTerm: ""}
-    }
-    onFormSubmit = (e) => {
-        e.preventDefault();
-        this.props.onSearchSubmit(this.state.searchTerm);
-    }
-    render(){
-        return (
-            <div className="ui segment" style={{marginTop: "1rem"}}>
-                <form className="ui form" onSubmit={this.onFormSubmit}>
-                    <div className="field">
-                        <label style={{textAlign: "center"}}>An Unbiased Youtube Search</label>
-                        <input 
-                            style={{textAlign: "center"}}
-                            type="text"
-                            placeholder="Start Typing . . . ."
-                            value={this.state.searchTerm}
-                            onChange={ e => this.setState({searchTerm: e.target.value})}
-                        />
-                    </div>
-                </form>
-                
-            </div>
-        )
-    }
-}
+const SearchBar = ({ onSearchSubmit }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onSearchSubmit(searchTerm);
+  };
+  return (
+    <div className="ui segment" style={{ marginTop: "1rem" }}>
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label style={{ textAlign: "center" }}>
+            An Unbiased Youtube Search
+          </label>
+          <input
+            style={{ textAlign: "center" }}
+            type="text"
+            placeholder="Start Typing . . . ."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
