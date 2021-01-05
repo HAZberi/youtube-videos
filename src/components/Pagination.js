c8666392818e4efca69b640729b0b76c7c1d5375
HAZberi <mhazuberi@gmail.com>
2021-01-05 06:06:19 -0700

    pagination conversion to hooks

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,56 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class Pagination extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { gotoPage: 1 };
-  }
-  totalPages({ videoList, resultsPerPage }) {
+const Pagination = (props) => {
+  const [gotoPage, setGotoPage] = useState(1);
+  const { displayNewList } = props;
+
+  const totalPages = ({ videoList, resultsPerPage }) => {
     return Math.ceil(videoList.length / resultsPerPage);
-  }
-  handleClickEvent = (e) => {
-    this.setState({ gotoPage: +e.target.dataset.target }, this.renderNewList);
+  };
+  const handleClickEvent = (e) => {
+    setGotoPage(+e.target.dataset.target);
   };
 
-  renderNewList() {
-    this.props.displayNewList(this.state.gotoPage);
-  }
+  useEffect(() => {
+    displayNewList(gotoPage);
+  }, [gotoPage, displayNewList]);
 
-  buttonMarkup({ currentPage }, direction, buttonText) {
+  useEffect(()=>{
+    setGotoPage(1);
+  }, [props.videoList])
+
+  const buttonMarkup = ({ currentPage }, direction, buttonText) => {
     return (
       <div style={{ textAlign: "center" }}>
         <button
           data-target={
             direction === "right" ? currentPage * 1 + 1 : currentPage - 1
           }
-          onClick={this.handleClickEvent}
+          onClick={handleClickEvent}
           className={`ui ${direction} button`}
         >
           {buttonText}
         </button>
       </div>
     );
-  }
-  paginationLogic(props = this.props) {
-    if (props.currentPage === 1 && this.totalPages(props) > 1)
-      return this.buttonMarkup(props, "right", "Next");
-    if (
-      props.currentPage === this.totalPages(props) &&
-      this.totalPages(props) > 1
-    )
-      return this.buttonMarkup(props, "left", "Back");
-    if (props.currentPage > 1 && props.currentPage < this.totalPages(props))
+  };
+
+  const paginationLogic = (props) => {
+    if (props.currentPage === 1 && totalPages(props) > 1)
+      return buttonMarkup(props, "right", "Next");
+    if (props.currentPage === totalPages(props) && totalPages(props) > 1)
+      return buttonMarkup(props, "left", "Back");
+    if (props.currentPage > 1 && props.currentPage < totalPages(props))
       return (
         <div style={{ display: "flex", justifyContent: "center" }}>
-          {this.buttonMarkup(props, "left", "Back")}
-          {this.buttonMarkup(props, "right", "Next")}
+          {buttonMarkup(props, "left", "Back")}
+          {buttonMarkup(props, "right", "Next")}
         </div>
       );
     return <div></div>;
-  }
-  render() {
-    return this.paginationLogic();
-  }
-}
+  };
+  return paginationLogic(props);
+};
 
 export default Pagination;
