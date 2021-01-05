7068a8f1169896952541b4fa1ace9a6dc260c0fe
HAZberi <mhazuberi@gmail.com>
2021-01-04 20:05:51 -0700

    Ap Component Refactored

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
 import "semantic-ui-css/semantic.min.css";
 import VideoList from "./VideoList";
 import VideoDetail from "./VIdeoDetail";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      videoList: [],
-      selectedVideo: null,
-    };
-  }
-  componentDidMount() {
-    this.searchResults("canada news");
-  }
-  searchResults = async (searchTerm) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: searchTerm,
-        safeSearch: "none",
-      },
-    });
-    this.setState({
-      videoList: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
+const App = () => {
+  const [videoList, setVideoList] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("canada news");
+
+  const getSearchResults = (searchTerm) => {
+    setSearchTerm(searchTerm);
   };
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onSearchSubmit={this.searchResults} />
-        <div className="ui stackable grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                videos={this.state.videoList}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
+  };
+
+  useEffect(() => {
+    const searchResults = async (searchTerm) => {
+      const response = await youtube.get("/search", {
+        params: {
+          q: searchTerm,
+          safeSearch: "none",
+        },
+      });
+      setVideoList(response.data.items);
+      setSelectedVideo(response.data.items[0]);
+    };
+    searchResults(searchTerm);
+  }, [searchTerm]);
+
+  return (
+    <div className="ui container">
+      <SearchBar onSearchSubmit={getSearchResults} />
+      <div className="ui stackable grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList
+              videos={videoList}
+              onVideoSelect={onVideoSelect}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
