027ced52c2922d45a70ef8c1be4700151351ddfc
Hassaan Zuberi <mhazuberi@gmail.com>
2021-05-08 21:15:46 -0600

    defined environmental variable for API key

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyC_BRssZllKbA0LDiZ0PwCPnBYEpP9W0OE";
+const KEY = `${process.env.REACT_APP_YOUTUBE_API_KEY}`;
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
     params: {
